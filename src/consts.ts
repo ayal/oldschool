82d8e3a71421c4594e106052ec96edfb3b180087
ayal <ayalg@wix.com>
2025-01-10 16:12:23 +0200

    changes

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'The Old School';
+export const SITE_DESCRIPTION = 'Old School is the nuschool';
