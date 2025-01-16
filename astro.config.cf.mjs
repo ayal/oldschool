aca697f1bd7fd6cfebd39b4cf1257b9404a99102
ayal <ayalg@wix.com>
2025-01-16 17:17:52 +0200

    build

--- astro.config.cf.mjs
+++ astro.config.cf.mjs
@@ -7,8 +7,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://ayal.github.io/oldschool',
-	base: '/olschool/',
+	site: 'https://thenuschool.com',
+	base: '/',
 	integrations: [mdx(), sitemap(), react(), tailwind()],
 	build: {
 		assets: 'assets'
